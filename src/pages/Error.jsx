2dae39c7161b5dd1c850d40e17ab2a6ff25a5e52
HarshavKoka <kokaharsha2203@gmail.com>
2025-02-15 23:56:11 +0530

    continuing the project

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-
 import { Link, useRouteError } from "react-router-dom";
 import img from "../assets/404.svg";
 import Wrapper from "../assets/wrappers/ErrorPage";
+
 const Error = () => {
   const error = useRouteError();
   console.log(error);
 
-  if (error.status === 404) {
+  if (error?.status === 404) {
     return (
       <Wrapper>
         <div>
-          <img src={img} />
+          <img src={img} alt="Page Not Found" />
           <h3>Ohh !</h3>
-          <p>We can't seem to find page your are looking for</p>
+          <p>We can't seem to find the page you are looking for.</p>
           <Link to="/">Back Home</Link>
         </div>
       </Wrapper>
@@ -23,7 +23,9 @@ const Error = () => {
   return (
     <Wrapper>
       <div>
-        <h3>something went wrong</h3>
+        <h3>Something went wrong</h3>
+        <p>Please try again later.</p>
+        <Link to="/">Back Home</Link>
       </div>
     </Wrapper>
   );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,24 @@
+import axios from "axios";
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLoaderData } from "react-router";
+import CocktailList from "../components/CocktailList";
+const cocktailSearchUrl =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+export const loader = async () => {
+    const searchQuery = "margarita";
+    const response = await axios.get(`${cocktailSearchUrl}${searchQuery}`);
+
+    return { drinks: response.data.drinks, searchQuery };
+};
 
 const Landing = () => {
+    const { drinks, searchQuery } = useLoaderData();
+    console.log(drinks);
     return (
-        <div>
-            <h1>landing</h1>
-            <Outlet />
-        </div>
+        <>
+            <CocktailList drinks={drinks} />
+        </>
     );
 };
 
